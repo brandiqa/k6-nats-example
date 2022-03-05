ef20e3fb857a8d960896b6de88722e0b36496f0f
Michael Wanyoike <micwanyoike@gmail.com>
2022-03-05 16:34:41 +0300

    Initial draft

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # k6-nats-example
+
+## Build
+
+To build a `k6` binary with this extension, first ensure you have the prerequisites:
+
+- [Go toolchain](https://go101.org/article/go-toolchain.html)
+- Git
+
+1. Install `xk6` framework for extending `k6`:
+
+   ```shell
+   go install go.k6.io/xk6/cmd/xk6@latest
+   ```
+
+2. Build the binary:
+
+   ```shell
+   xk6 build --with github.com/ydarias/xk6-nats@latest
+   ```
+
+## Run
+
+Run a test
+
+```shell
+./k6 run folder/test.js
+```
---
+++ scripts/test.js
@@ -0,0 +1,35 @@
+import { check, sleep } from "k6";
+import { Nats } from "k6/x/nats";
+
+const natsConfig = {
+  servers: ["nats://localhost:4222"],
+  unsafe: true,
+};
+
+const publisher = new Nats(natsConfig);
+const subscriber = new Nats(natsConfig);
+
+// export const options = {
+//   duration: "10s",
+//   vus: 10,
+// };
+
+export default function () {
+  subscriber.subscribe("topic", (msg) => {
+    check(msg, {
+      "Is expected message": (m) => m.data === "the message",
+      "Is expected topic": (m) => m.topic === "topic",
+    });
+  });
+
+  sleep(1);
+
+  publisher.publish("topic", "the message");
+
+  sleep(1);
+}
+
+export function teardown() {
+  publisher.close();
+  subscriber.close();
+}
